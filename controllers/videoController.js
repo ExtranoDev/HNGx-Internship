008c88c84cd9da9aaced00a5952d15ad87f7cf45
extranodev <jeomade19@gmail.com>
2023-10-01 16:58:30 +0100

    Added some error handlign features

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -31,43 +31,58 @@ const convert2audio = (videoPath, outputPath) => {
 }
 
 const saveChunckVideo = async (req, res) => {
-    const fileData = req.file
-    await videoChunks.push(fileData.buffer)
+    try {
+        const fileData = req.file
+        await videoChunks.push(fileData.buffer)
+    } catch (error) {
+        throw new BadRequestError("Invalid Blob file")
+    }
+
 }
 
 const startVideo = (req, res) => {
-    videoChunks = [] // clears previous video
-    // await recorder.startRecording()
-    videoID = Math.floor(Math.random() * 9000000000) + 1000000000 //generated video ID
-    res.status(200).json({ "msg": "Video begins", videoID })
+    try {
+        videoChunks = [] // clears previous video
+        // await recorder.startRecording()
+        videoID = Math.floor(Math.random() * 9000000000) + 1000000000 //generated video ID
+        res.status(200).json({ "msg": "Video begins", videoID })
+    } catch (error) {
+        res.status(401).json({ "msg": "Unable to start Video" })
+    }
+
 }
 
 const pauseVideo = async (req, res) => {
-    // await recorder.pauseRecording(async () => {
-    //     const blob = recorder.getBlob()
-    // })
-    await saveChunckVideo(req, res)
-    res.status(200).json({ "msg": "Video paused", videoID })
+    try {
+        await saveChunckVideo(req, res)
+        res.status(200).json({ "msg": "Video paused", videoID })
+    } catch (error) {
+        throw new BadRequestError("Unable to pause video")
+    }
 }
 
 const resumeVideo = (req, res) => {
-    // await recorder.resumeRecording()
     res.status(200).json({ "msg": "Video resumed", videoID })
 }
 
 const stopVideo = async (req, res) => {
-    await saveChunckVideo(req, res)
-    const videoBuffer = await Buffer.concat(videoChunks)
-    const folderPath = path.join(__dirname, 'videos', `${videoID}`)
-    console.log(folderPath)
+    try {
+        await saveChunckVideo(req, res)
+        const videoBuffer = await Buffer.concat(videoChunks)
+        const folderPath = path.join(__dirname, 'videos', `${videoID}`)
+        console.log(folderPath)
 
-    const videoPath = folderPath + '.webm'
+        const videoPath = folderPath + '.webm'
 
-    await fs.writeFileSync(videoPath, videoBuffer)
-    videoChunks = []
+        await fs.writeFileSync(videoPath, videoBuffer)
+        videoChunks = []
 
-    await getTranscribe(folderPath, videoID)
-    res.status(200).json({ "msg": "Video stopped", videoID })
+        await getTranscribe(folderPath, videoID)
+        res.status(200).json({ "msg": "Video stopped", videoID })
+    } catch (error) {
+        throw new BadRequestError("Unable to stop video an error occured")
+    }
+
 }
 
 const getTranscribe = async (folderPath, id) => {
@@ -103,33 +118,37 @@ const getTranscribe = async (folderPath, id) => {
 }
 
 const getVideo = async (req, res) => {
-    const { id } = req.params
-    const range = req.headers.range
-    folderPath = path.join(__dirname, `videos/${id}`)
-    filePath = folderPath + '.mp4'
+    try {
+        const { id } = req.params
+        const range = req.headers.range
+        folderPath = path.join(__dirname, `videos/${id}`)
+        filePath = folderPath + '.mp4'
 
-    const videoSize = fs.statSync(filePath).size
-    const chunkSize = 1 * 1e6;
-    const start = Number(range.replace(/\D/g, ""))
-    const end = Math.min(start + chunkSize, videoSize - 1)
-    const videoLength = end - start + 1;
+        const videoSize = fs.statSync(filePath).size
+        const chunkSize = 1 * 1e6;
+        const start = Number(range.replace(/\D/g, ""))
+        const end = Math.min(start + chunkSize, videoSize - 1)
+        const videoLength = end - start + 1;
 
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": videoLength,
-        "Content-Type": "video/mp4"
+        const headers = {
+            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": videoLength,
+            "Content-Type": "video/mp4"
+        }
+        res.writeHead(206, headers)
+        const stream = await fs.createReadStream(filePath, { start, end })
+
+        // res.end({
+        //     "status": 'Video acced',
+        //     "videoPath": `${folderPath}.mp4`,
+        //     "transcription": transData.results
+        // })
+
+        stream.pipe(res)
+    } catch (NotFoundError) {
+        throw new NotFoundError("Can't find the ID'ied video")
     }
-    res.writeHead(206, headers)
-    const stream = await fs.createReadStream(filePath, { start, end })
-
-    // res.end({
-    //     "status": 'Video acced',
-    //     "videoPath": `${folderPath}.mp4`,
-    //     "transcription": transData.results
-    // })
-
-    stream.pipe(res)
 }
 
 module.exports = {
